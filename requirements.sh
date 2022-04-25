958b77f08d1cff0200231e6b5047c9fabc318718
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-04-25 13:05:13 -0400

    Dockerfile para intanciar y crear una base de datos en postgres listo

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+conda create --name fastapi python=3.10 -y
+
+conda activate fastapi
+
 conda install -c conda-forge fastapi -y
 conda install -c conda-forge uvicorn -y
 conda install -c conda-forge tortoise-orm -y
