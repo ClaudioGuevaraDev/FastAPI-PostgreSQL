d1145696712c3a376cf9d9dfe85d21430580136b
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-04-25 13:50:06 -0400

    get a task, ready

--- server/models/Task.py
+++ server/models/Task.py
@@ -1,4 +1,18 @@
+from lib2to3.pgen2.token import OP
 from pydantic import BaseModel
+from typing import Optional
 
 class TaskModel(BaseModel):
-    title: str
+    id: Optional[int] = None
+    title: str 
+    created_at: Optional[str] = None
+    updated: Optional[str] = None
+
+class TaskModelGetResponse(BaseModel):
+    status: str
+    data: list[TaskModel]
+
+class TaskModelPostResponse(BaseModel):
+    status: str
+    message: str
+    data: TaskModel
--- server/routes/tasks.py
+++ server/routes/tasks.py
@@ -1,18 +1,17 @@
-from urllib import response
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 
 from database.models import Task
-from models.Task import TaskModel
+from models.Task import TaskModel, TaskModelGetResponse, TaskModelPostResponse
 
 router = APIRouter(
     tags=["Tasks"],
     prefix="/api/tasks"
 )
 
-@router.get("/", status_code=status.HTTP_200_OK, description="Get all tasks")
+@router.get("/", status_code=status.HTTP_200_OK, description="Get all tasks", response_model=TaskModelGetResponse)
 async def get_tasks():
-    tasks = await Task.all()
+    tasks: list[Task] = await Task.all()
 
     response = jsonable_encoder({
         "status": "success",
@@ -21,7 +20,25 @@ async def get_tasks():
 
     return response
 
-@router.post("/", status_code=status.HTTP_201_CREATED, description="Create a new task")
+@router.get("/:id", status_code=status.HTTP_200_OK)
+async def get_task(id: int):
+    try:
+        task: Task = await Task.get(id=id)
+    except:
+        response = jsonable_encoder({
+            "status": "error",
+            "message": "Task not found"
+        })
+        raise HTTPException(detail=response, status_code=status.HTTP_404_NOT_FOUND)
+
+    response = jsonable_encoder({
+        "status": "success",
+        "data": task
+    })
+
+    return response
+
+@router.post("/", status_code=status.HTTP_201_CREATED, description="Create a new task", response_model=TaskModelPostResponse)
 async def create_task(task: TaskModel):
     new_task = await Task.create(title=task.title)
 
